0a2ef2bb17ecd61af71b4f5284b7916358d89120
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-08-26 12:38:44 +0530

    Update hollow rect.cpp

--- hollow rect.cpp
+++ hollow rect.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //hollow rect
 #include<iostream>
 using namespace std;
@@ -16,3 +17,23 @@ int main(){
 	}
 	return 0;
 }
+=======
+//hollow rect
+#include<iostream>
+using namespace std;
+int main(){
+	int row,col;
+	cin>>row>>col;
+	for(int i=1;i<=row;i++){
+		for(int j=1;j<=col;j++){
+			if(i==1 || i==row || j==1 || j==col){
+				cout<<"*";
+			}
+			else
+				cout<<" ";
+		} 
+		cout<<endl;
+	}
+	return 0;
+}
+>>>>>>> fdd35bca5c1207c7f05fb71c13b254a685367170
