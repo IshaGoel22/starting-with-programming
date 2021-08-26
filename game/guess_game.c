8dcf57e83ce350e7a1c77e7ed5a8b662f9442480
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-08-26 12:31:42 +0530

    Update guess_game.c

--- game/guess_game.c
+++ game/guess_game.c
@@ -4,8 +4,18 @@
 
 int main(){
     srand(time(0));    //initializing
-    int num;
+    int num,guess;
     num = rand()%100 + 1;
     //generate random number between 1 and 100
+    do
+    {
+        printf("Guess the number between 1 to 100");
+        scanf(%d,&guess);
+        if(guess>num){
+            printf("Lower number please\n");
+        }
+        else
+    } while ();
+    
     printf("The random number is %d",num);
 }
