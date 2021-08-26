726b95d9598cadffabb2cb5dd217f629a83b7cde
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2021-08-26 23:56:40 +0530

    Update calloc.c

--- calloc.c
+++ calloc.c
@@ -7,6 +7,10 @@ int main(){
      for(int i=0;i<6;i++){
         printf("The value at location %d is %d\n",i,ptr[i]);
     }
+    //if space is not there, null ptr is returned --> good to free memory
     //calloc gives 0 as default value at every location where as malloc do not
+    //free(ptr) --> to de allocate memory
+    //realloc --> to allocate memory of new size using previous pointer and size
+    // realloc(ptr,10*sizeof(int));  --> reallocating ptr memory to storing 10 int
     return 0;
 }
